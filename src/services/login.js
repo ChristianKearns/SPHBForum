c070d6b8b02fcb087f70fff2c88c94ba55955700
ChristianKearns <80787936+ChristianKearns@users.noreply.github.com>
2023-02-16 03:45:07 -0500

    Delete src directory

--- src/services/login.js
+++
@@ -1,8 +0,0 @@
-import { makeRequest } from "./makeRequest";
-
-export function login({ id, name }) {
-    return makeRequest(`/login`, {
-        method: "POST",
-        data: { id, name },
-    })
-}
